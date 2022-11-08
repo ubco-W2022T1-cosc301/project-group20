a2b8891edf852fbc9fbc51eccce679c5d938f4b1
Gerren Hunter <44909431+CookiedOutMonster@users.noreply.github.com>
2022-11-08 14:15:31 -0800

    Update functions2.py

--- notebooks/functions2.py
+++ notebooks/functions2.py
@@ -1,1 +1,25 @@
+# Gerren Method Chaining 
+import pandas as pd
 
+def load_and_process(file): 
+  
+  #load csv into dataframe 
+  df = pd.read_csv(file)
+  
+  #clean data 
+  
+   df_processed = pd.concat(
+    [df['B_avg_opp_HEAD_landed'],df['B_wins'],df['B_win_by_Decision_Majority'],df['B_win_by_Decision_Split'],df['B_win_by_Decision_Unanimous'],df['B_win_by_KO/TKO'],
+    df['B_win_by_Submission'],df['B_win_by_TKO_Doctor_Stoppage'],  [df['R_avg_opp_HEAD_landed'],df['R_wins'],df['R_win_by_Decision_Majority'],df['R_win_by_Decision_Split'],df['R_win_by_Decision_Unanimous'],df['R_win_by_KO/TKO'],
+    df['R_win_by_Submission'],df['R_win_by_TKO_Doctor_Stoppage'],
+    axis = 1
+    )
+                                                                    
+                                                                    
+                                                                   
+   return df_processed.dropna()
+                                                                    
+                                                                    
+    
+    
+  
